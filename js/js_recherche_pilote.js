62a8fa084ab8e33bf86357325b05dc2ea79e0293
aumaxim <maxime.auchet@viacesi.fr>
2024-03-12 16:32:37 +0100

    Pop du tableau pour modifier et supprimer

--- js/js_recherche_pilote.js
+++ js/js_recherche_pilote.js
@@ -53,6 +53,96 @@ var myArray = [
     promo: "Promotion 2021",
     plus: "Wesh",
   },
+  {
+    numero: "5",
+    nom: "Thomas",
+    prenom: "Luc",
+    centre: "Centre 2",
+    mail: "luc.thomas@example.com",
+    promo: "Promotion 2023",
+    plus: "Wesh",
+  },
+  {
+    numero: "6",
+    nom: "Petit",
+    prenom: "Alice",
+    centre: "Centre 3",
+    mail: "alice.petit@example.com",
+    promo: "Promotion 2021",
+    plus: "Wesh",
+  },
+  {
+    numero: "5",
+    nom: "Thomas",
+    prenom: "Luc",
+    centre: "Centre 2",
+    mail: "luc.thomas@example.com",
+    promo: "Promotion 2023",
+    plus: "Wesh",
+  },
+  {
+    numero: "6",
+    nom: "Petit",
+    prenom: "Alice",
+    centre: "Centre 3",
+    mail: "alice.petit@example.com",
+    promo: "Promotion 2021",
+    plus: "Wesh",
+  },
+  {
+    numero: "5",
+    nom: "Thomas",
+    prenom: "Luc",
+    centre: "Centre 2",
+    mail: "luc.thomas@example.com",
+    promo: "Promotion 2023",
+    plus: "Wesh",
+  },
+  {
+    numero: "6",
+    nom: "Petit",
+    prenom: "Alice",
+    centre: "Centre 3",
+    mail: "alice.petit@example.com",
+    promo: "Promotion 2021",
+    plus: "Wesh",
+  },
+  {
+    numero: "5",
+    nom: "Thomas",
+    prenom: "Luc",
+    centre: "Centre 2",
+    mail: "luc.thomas@example.com",
+    promo: "Promotion 2023",
+    plus: "Wesh",
+  },
+  {
+    numero: "6",
+    nom: "Petit",
+    prenom: "Alice",
+    centre: "Centre 3",
+    mail: "alice.petit@example.com",
+    promo: "Promotion 2021",
+    plus: "Wesh",
+  },
+  {
+    numero: "5",
+    nom: "Thomas",
+    prenom: "Luc",
+    centre: "Centre 2",
+    mail: "luc.thomas@example.com",
+    promo: "Promotion 2023",
+    plus: "Wesh",
+  },
+  {
+    numero: "6",
+    nom: "Petit",
+    prenom: "Alice",
+    centre: "Centre 3",
+    mail: "alice.petit@example.com",
+    promo: "Promotion 2021",
+    plus: "Wesh",
+  },
 ];
 
 buildTable(myArray);
@@ -68,16 +158,27 @@ function buildTable(data) {
                     <td>${data[i].centre}</td>
                     <td>${data[i].mail}</td>
                     <td>${data[i].promo}</td>
-                    <td> <img class="imgautre" src="image/autre.png"></td>
-
-
+                    <td><img class="imgautre" src="image/autre.png"></td>
               </tr>`;
     table.innerHTML += row;
   }
+  attachImageEventListeners();
 }
-
+function openPopup() {
+  let popup = document.getElementById("popupautre");
+  popup.style.display = "block";
+  document.body.classList.remove("disable-scroll");
+}
+function closePopup() {
+  let popup = document.getElementById("popupautre");
+  popup.style.display = "none";
+}
 function openPopup1() {
   let popup = document.getElementById("popupajout1");
+  popup.classList.toggle("open");
+}
+function openPopup2() {
+  let popup = document.getElementById("popupmodifier");
   popup.classList.toggle("open");
 }
 
@@ -101,3 +202,48 @@ function searchTable(value, data) {
   }
   return filteredData;
 }
+function attachImageEventListeners() {
+  document.querySelectorAll(".imgautre").forEach((img) => {
+    img.addEventListener("click", function (event) {
+      openPopup();
+      var rect = img.getBoundingClientRect(); // Obtient les dimensions et la position de l'image par rapport à la fenêtre visible
+      var x = rect.left + window.scrollX; // Ajoute le décalage horizontal de défilement
+      var y = rect.bottom + window.scrollY; // Ajoute le décalage vertical de défilement
+
+      console.log(x);
+      console.log(y);
+
+      // Show the popup and position it below the clicked image
+      let popup = document.getElementById("popupautre");
+      popup.style.display = "block";
+      popup.style.top = y + "px";
+      popup.style.left = x + "px";
+    });
+  });
+}
+attachImageEventListeners();
+
+document.addEventListener("click", function (event) {
+  var popup = document.getElementById("popupautre");
+  var imgautre = document.getElementsByClassName("imgautre");
+
+  // Vérifie si l'élément cliqué est une image
+  var isImageClicked = false;
+  for (var i = 0; i < imgautre.length; i++) {
+    if (event.target === imgautre[i]) {
+      isImageClicked = true;
+      break;
+    }
+  }
+
+  // Si l'élément cliqué n'est pas une image, fermer le popup
+  if (!isImageClicked && event.target !== popup) {
+    closePopup();
+  }
+});
+
+document
+  .querySelector(".table-container")
+  .addEventListener("scroll", function (event) {
+    closePopup();
+  });
