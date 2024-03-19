03fdb42c27b6d29f0e6ca6ed20d55fa6fbfb49c2
aumaxim <maxime.auchet@viacesi.fr>
2024-03-19 11:15:08 +0100

    champ préremplis quand on  clique sur modifier le pilotes

--- gerer_pilotes.php
+++ gerer_pilotes.php
@@ -45,7 +45,7 @@
           </div>
         </div>
         <div class="table-container">
-          <table>
+          <table id="tabinfo">
             <tr>
               <th class="bordure">#</th>
               <th class="bordure">Nom</th>
@@ -89,7 +89,7 @@
             <option value="centre2">Centre 2</option>
             <option value="centre3">Centre 3</option>
           </select>
-          <select id="promotion" name="promotion">
+          <select id="promotion" name="promotion">  
             <option value="" disabled selected>Promotion</option>
             <option value="promotion1">Promotion 1</option>
             <option value="promotion2">Promotion 2</option>
