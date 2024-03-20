16868b4a439320de950ce71b5a2ce69464c5f316
aumaxim <maxime.auchet@viacesi.fr>
2024-03-20 14:22:53 +0100

    Premier test de se connecter avec la BDD

--- Se_Connecter.php
+++ Se_Connecter.php
@@ -17,28 +17,64 @@
   <body>
   <?php include 'Navbar.php'; ?>
 
+  <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
+$pseudo = $_POST['email'];
+$mot_de_passe = $_POST['password'];
+$username= "sql11692929"; 
+$password= "lBxIuH8Sie";
+try {
+    $db = new PDO('mysql:host=sql11.freesqldatabase.com;dbname=sql11692929', $username, $password);
+    }
+ catch (PDOException $e) {
+    echo "Erreur lors de la connexion à la base de données : " . $e->getMessage();
+}
+
+$query = $db->prepare("SELECT * FROM admin WHERE username=:pseudo AND password=:mot_de_passe");
+$query->bindParam(':pseudo', $pseudo);
+$query->bindParam(':mot_de_passe', $mot_de_passe);
+$query->execute();
+
+if ($query->rowCount() > 0) {
+    $row = $query->fetch(PDO::FETCH_ASSOC);
+    $id_utilisateur = $row['id'];
+    session_start();
+    $_SESSION['username'] = $row['username'];
+    header("location:/Page_acceuil.php");
+} else {
+    echo "Identifiants incorrects.";
+}
+}
+
+?>
     <div class="box">
       <div class="box_connexion">
         <div class="box_connexion_contenu">
+          <form action="" method="post">
           <label class="text_connexion">Se connecter</label>
           <input
             class="email"
-            type="email"
+            type="text"
             id="email"
+            name="email"
             placeholder="Adresse email"
+            required
           />
           <div class="box_password">
             <input
               class="password"
               type="password"
               id="password"
+              name="password"
               placeholder="Mot de passe"
             />
             <button class="btn_password">afficher</button>
           </div>
           <a href="Changer_mdp.html">Mot de passe oublié ?</a>
-          <button class="btn_connexion">Se connecter</button>
+          <button type="submit" class="btn_connexion">Se connecter</button>
+          </form>
+
         </div>
       </div>
     </div>
